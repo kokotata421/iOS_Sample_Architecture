98d5a7b0d7c636d4380c5d11e711cde3075fe9ad
Kota Kawanishi <kokotata421@gmail.com>
2022-08-30 01:09:22 +0900

    [add 🚀] UIKit_Sample_Architecture Project

---
+++ UIKit_Sample_Architecture/UIKit_Sample_Architecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Sample_Architecture
+//
+//  Created by Kota Kawanishi on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
